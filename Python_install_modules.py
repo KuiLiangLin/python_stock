fc212daa68f13047b336e05ffaee43d5aa68bfed
Kuiliang Lin <b9907020@mail.ntust.edu.tw>
2017-11-14 21:30:56 +0800

    Add files via upload

--- Python_install_modules.py
+++ Python_install_modules.py
@@ -1,23 +1,31 @@
+import os
+
+os.system('cd ~')
+os.system('cd Users')
+os.system('cd Lin')
+os.system('cd AppData')
+os.system('cd Local')
+os.system('cd Programs')
+os.system('cd Python')
+os.system('cd Scripts')
+
+os.system('python get-pip.py')
 
 
-cd ~
-cd Users
-cd Lin
-cd AppData
-cd Local
-cd Programs
-cd Python
-
-
-python get-pip.py
-
-
-pip list
+os.system('pip list')
 
 #for vision 3.x.x
-pip install -vvv requests
+os.system('pip install -vvv requests')
+os.system('pip install -vvv pandas')
+os.system('pip install -vvv lxml')
+os.system('pip install -vvv html5lib')
+os.system('pip install -vvv describe')
+os.system('pip install -vvv matplotlib')
+os.system('pip install -vvv sklearn')
+os.system('pip install -vvv jupyter')
 
-pip install -vvv pandas
+
+
 
 
 
